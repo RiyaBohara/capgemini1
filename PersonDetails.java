29e57c361984d62bb1c36b65a5cc7c3961545e98
Riya Bohara <boharariya@gmail.com>
2020-01-30 16:48:25 +0530

    Added PersonDetails.java File

---
+++ PersonDetails.java
@@ -0,0 +1,13 @@
+package lab2_1;
+
+public class PersonDetails {
+
+	public static void main(String[] args) {
+		System.out.println("Name is: Divya");
+		System.out.println("Name is: Bharati");
+		System.out.println("Gender : F");
+		System.out.println("Age : 20");
+		System.out.println("Weight : 55.55");
+	}
+
+}
